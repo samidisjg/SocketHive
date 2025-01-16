d849b0e557793800565ed10f2801c58bd4ea71da
Samidi Jayawickrama <107723505+samidisjg@users.noreply.github.com>
2025-01-16 15:13:22 +0530

    initial commit1

---
+++ convostream/src/test/java/com/sami/convostream/ConvostreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sami.convostream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvostreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
